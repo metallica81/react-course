de2dc065567777d9257662058a5acd5e0f96cd8f
metallica81 <metallica923@gamil.com>
2025-03-12 21:41:18 +0300

    refactor: transfer from js syntax to jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,25 +5,28 @@ import { restaurants } from "./mock.js";
 
 const root = document.getElementById("root");
 createRoot(root).render(
-    restaurants.map((element) => {
-        return createElement("div", {style: {"padding-left": "30px"}}, [
-            createElement("h1", {}, element.name),
-            createElement("h3", {}, "Меню"),
-            createElement(
-                "ul",
-                {},
-                element.menu.map((element) => {
-                    return createElement("li", {}, element.name);
-                })
-            ),
-            createElement("h3", {}, "Отзывы"),
-            createElement(
-                "ul",
-                {},
-                element.reviews.map((element) => {
-                    return createElement("li", {}, element.text);
-                })
-            ),
-        ]);
+    restaurants.map((restaurant) => {
+        return (
+            <div style={{"padding-left": "30px"}}>
+                <h2>{restaurant.name}</h2>
+                <h3>Меню</h3>
+                <ul>
+                    {
+                        restaurant.menu.map((menu) => {
+                            return <li>{menu.name}</li>;
+                        })
+                    }
+                </ul>
+
+                <h3>Отзывы</h3>
+                <ul>
+                    {
+                        restaurant.reviews.map((reviews) => {
+                            return <li>{reviews.text}</li>;
+                        })
+                    }
+                </ul>
+            </div>
+        )
     })
 );
