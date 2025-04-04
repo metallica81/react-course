6409355eb3801c5ba2532e70fb4b1f176f1284b8
metallica81 <metallica923@gamil.com>
2025-04-04 21:41:58 +0300

    try to request data in second page

--- src/Pages/RestaurantPage/RestaurantPage.jsx
+++ src/Pages/RestaurantPage/RestaurantPage.jsx
@@ -1,28 +1,19 @@
 import { Outlet, useParams } from "react-router";
 import { RestaurantContainer } from "../../components/Restaurant/RestaurantContainer";
-import { useRequest } from "../../Redux/Hooks/useRequest";
 import { useSelector } from "react-redux";
+import { selectRestaurantById, selectRestaurantIds } from "../../Redux/Entities/Restaurant/slice";
+import { useRequest } from "../../Redux/Hooks/useRequest";
 import { getRestaurants } from "../../Redux/Entities/Restaurant/getRestaurant";
-import { selectRestaurantIds } from "../../Redux/Entities/Restaurant/slice";
-import { useRestaurantPage } from "../RestaurantsTabsPage/useRestruantPage";
 
 export function RestaurantPage() {
     const { restaurantId } = useParams();
 
     const requestStatus = useRequest(getRestaurants);
+    console.log('requestStatus', requestStatus)
 
     const restaurantIds = useSelector(selectRestaurantIds);
-
-    const { activeRestaurantId, handleChooseRestaurant } =
-        useRestaurantPage(restaurantIds);
-
-    if (requestStatus === "idle" || requestStatus === "pending") {
-        return "loading...";
-    }
-
-    if (requestStatus === "rejected") {
-        return "error";
-    }
+    const restaurantInfo = useSelector((state) => selectRestaurantById(state, restaurantId));
+    console.log('restaurantInfo', restaurantInfo)
 
     return (
         <>
